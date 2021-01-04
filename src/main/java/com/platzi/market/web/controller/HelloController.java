7e795c7e03403fbe5e96d184861ad38ffb50716b
JairKevinFG <jkfloresg@uni.pe>
2021-01-03 22:08:42 -0500

    properties

--- src/main/java/com/platzi/market/web/controller/HelloController.java
+++ src/main/java/com/platzi/market/web/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.platzi.market;
+package com.platzi.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,7 @@ public class HelloController {
 
     @GetMapping("/hola")
     public String saludar(){
-        return "Nunca pares de aprender";
+        return "hola mundo";
     }
 }
+
